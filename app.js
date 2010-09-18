4c66a8498ce676de31b9483403363480c46d4b8a
Jay Adkisson <j4yferd@gmail.com>
2010-09-18 11:27:42 -0700

    big huge ugly

--- app.js
+++ app.js
@@ -3,51 +3,42 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-    connect = require('connect'),
-    io = require('socket.io');
+var http = require('http'),
+    io = require('socket.io'),
+    ejs = require('ejs'),
+    fs = require('fs'),
+    sys = require('sys');
+    $dmp = new require('./google-diff-match-patch').diff_match_patch();
 
-var sys = require('sys');
-var app = module.exports = express.createServer();
+var server = http.createServer(function(req,res) {
+  fs.readFile('./public/index.html', function(err, data) {
+    if(err) throw err;
 
-// Configuration
-
-app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.use(connect.bodyDecoder());
-    app.use(connect.methodOverride());
-    app.use(app.router);
-    app.use(connect.staticProvider(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-   app.use(connect.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-    res.render('index.ejs', {
-        locals: {
-            title: 'Express'
-        }
-    });
-});
-
-var socket = io.listen(app); 
-socket.on('connection', function(client){ 
-  	client.on('message', function(){ 
-		sys.puts(sys.inspect(arguments)) 
-	});
-  	client.on('disconnect', function(){ 
-		sys.puts(sys.inspect(arguments))
-	});
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+  });
 });
 
 // Only listen on $ node app.js
 
-if (!module.parent) app.listen(3000);
+if (!module.parent) server.listen(3000);
+
+
+
+var socket = io.listen(server);
+
+var currentMessage = '';
+
+socket.on('connection', function(client){ 
+  client.send($dmp.make_patch('', currentMessage);
+
+  client.on('message', function(message){ 
+    sys.puts('message received: '+sys.inspect(message));
+    var patch = JSON.parse(message);
+    currentMessage = $dmp.patch_apply(patch, currentMessage)[0];
+    client.broadcast(message);
+  }); 
+  client.on('disconnect', function(){ 
+    sys.puts('disconnected: '+sys.inspect(arguments));
+  });
+});
