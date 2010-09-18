665d4c4881b135808a345c7529daa550d0d0c5af
Jay Adkisson <j4yferd@gmail.com>
2010-09-18 11:43:39 -0700

    another big ugly, but refreshing works

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ var http = require('http'),
     io = require('socket.io'),
     ejs = require('ejs'),
     fs = require('fs'),
-    sys = require('sys');
-    $dmp = new require('./google-diff-match-patch').diff_match_patch();
+    sys = require('sys'),
+    dmp = require('./google-diff-match-patch').diff_match_patch;
+
+var $dmp = new dmp();
+
+console.log(sys.inspect($dmp.__proto__));
 
 var server = http.createServer(function(req,res) {
   fs.readFile('./public/index.html', function(err, data) {
@@ -29,8 +33,8 @@ var socket = io.listen(server);
 
 var currentMessage = '';
 
-socket.on('connection', function(client){ 
-  client.send($dmp.make_patch('', currentMessage);
+socket.on('connection', function(client){
+  client.send(JSON.stringify($dmp.patch_make('', currentMessage)));
 
   client.on('message', function(message){ 
     sys.puts('message received: '+sys.inspect(message));
